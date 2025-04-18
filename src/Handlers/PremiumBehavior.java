0f2db270388f4e27ee0970ac0b62529c7c71c06a
Sarabasiroo <Sarabasiroo@yahoo.com>
2025-04-18 22:47:08 +0330

    User behaviors implemented

--- src/Handlers/PremiumBehavior.java
+++ src/Handlers/PremiumBehavior.java
@@ -1,4 +1,46 @@
 package Handlers;
 
-public class PremiumBehavior {
+public class PremiumBehavior implements UserBehavior {
+    private int month;
+
+    public PremiumBehavior(int month) {
+        this.setMonth(month);
+    }
+
+    @Override
+    public void createPlaylist(String title, User owner) {
+        if (owner == null) {
+            throw new InvalidOperationException("set the owner");
+        }
+        if (title == null || title.trim().isEmpty()) {
+            throw new InvalidOperationException("Enter a valid title");
+        }
+        Playlist playlist = new Playlist(title, owner);
+        owner.getPlaylists().add(playlist);
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if (music == null) {
+            throw new InvalidOperationException("Select the music");
+        }
+        music.play();
+    }
+
+    public void setMonth(int month) {
+        if (month <= 0)
+            throw new InvalidOperationException("invalid time format");
+
+        this.month = month;
+    }
+    @Override
+    public void buyPremium(User owner, int month) {
+        if (month <= 0) {
+            throw new InvalidOperationException("invalid time format(negative value).");
+        }
+        this.month += month;
+    }
+    public int getMonth() {
+        return month;
+    }
 }
--- src/Handlers/RegularBehavior.java
+++ src/Handlers/RegularBehavior.java
@@ -1,4 +1,25 @@
 package Handlers;
 
-public class RegularBehavior {
+public class RegularBehavior implements UserBehavior {
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist(String title, User owner) {
+        throw new InvalidOperationException("You discovered a premium feature. upgrade to make playlists");
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if (playingLimit <= 0) {
+            throw new InvalidOperationException("You discovered a premium feature. upgrade to play more songs");
+        }
+        music.play();
+        playingLimit--;
+    }
+
+    @Override
+    public void buyPremium(User owner, int len) {
+        owner.setBehavior(new PremiumBehavior(len));
+    }
+
 }
