ceea8442290a239bec2603d86cc1231776850843
Sarabasiroo <Sarabasiroo@yahoo.com>
2025-04-18 23:01:16 +0330

    Music and Playlist classes implemented

--- src/Handlers/Music.java
+++ src/Handlers/Music.java
@@ -1,4 +1,55 @@
 package Handlers;
+import java.util.*;
 
 public class Music {
+    private String title;
+    private User singer;
+    private int numberOfStream = 0;
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+
+    public Music(String title, User singer){
+        for (Music music: allMusics)
+            if (music.title.equals(title) && music.singer.equals(singer))
+                throw new InvalidOperationException("Music already exists!");
+        this.title = title;
+        this.singer = singer;
+        this.numberOfStream = 0;
+        allMusics.add(this);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void play(){
+        System.out.println("Playing :  '" + title + "' by :  " + singer.getUsername() + " is playing");
+        numberOfStream++;
+    }
+
+    public ArrayList<Music> search(String title) {
+        ArrayList<Music> foundMusics = new ArrayList<>();
+        for (Music music : allMusics) {
+            if (music.title.equalsIgnoreCase(title)) {
+                foundMusics.add(music);
+            }
+        }
+        if (foundMusics.isEmpty()){
+            return null;
+        }
+        else {
+            return foundMusics;
+        }
+    }
+
+    public Music search(String title, String singerName) {
+        for (Music music : allMusics) {
+            if (music.title.equalsIgnoreCase(title) && music.singer.getUsername().equalsIgnoreCase(singerName)) {
+                return music;
+            }
+        }
+        return null;
+    }
+    public User getSinger() { return singer; }
+    public int getNumberOfStream() { return numberOfStream; }
+    public static ArrayList<Music> getAllMusics() { return allMusics;}
 }
--- src/Handlers/Playlist.java
+++ src/Handlers/Playlist.java
@@ -1,4 +1,77 @@
 package Handlers;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Playlist {
+    private String title;
+    private ArrayList<Music> playlist;
+    private User owner;
+
+    public Playlist(String title, User owner) {
+        this.title = title;
+        this.owner = owner;
+        this.playlist = new ArrayList<>();
+    }
+
+    public void addMusic(Music music, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Incorrect Password.");
+        if (playlist.contains(music))
+            throw new InvalidOperationException("this song is already added.");
+        playlist.add(music);
+    }
+
+    public void removeMusic(Music music, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if (!playlist.contains(music))
+            throw new InvalidOperationException("Music not found in playlist");
+        playlist.remove(music);
+        System.out.println("\nMusic removed from playlist: " + music.getTitle());
+    }
+
+    public void editTitle(String newTitle, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Incorrect Password.");
+        this.title = newTitle;
+    }
+
+    public ArrayList<Music> searchInPlaylist(String title) {
+        ArrayList<Music> result = new ArrayList<>();
+        for (Music music : playlist) {
+            if (music.getTitle().equals(title)) result.add(music);
+        }
+        if (result.isEmpty()){
+            return null;
+        }
+        else {
+            return result;
+        }
+    }
+
+    public Music searchInPlaylist(String title, User singer) {
+        for (Music m : playlist) {
+            if (m.getTitle().equals(title) && m.getSinger().equals(singer)){
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void playPlaylist() {
+        for (Music music : playlist) {
+            music.play();
+        }
+    }
+
+    public void shuffle() {
+        ArrayList<Music> shuffled = new ArrayList<>(playlist);
+        Collections.shuffle(shuffled);
+        for (Music music : shuffled) {
+            music.play();
+        }
+    }
+    public String getTitle() { return title; }
+    public ArrayList<Music> getPlaylist() { return playlist; }
+    public User getOwner() { return owner; }
 }
